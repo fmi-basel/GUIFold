2111e177fbdfcfa0b812139a0659895e4cf881b7
georg.kempf <geox19@gmx.de>
2022-06-24 13:30:41 +0200

    Fix jax source in setup script.

--- setup_environment.sh
+++ setup_environment.sh
@@ -8,7 +8,7 @@ cd $base_dir && \
 conda env create -p $(pwd)/$CONDA_ENV_NAME -f GUIFold/conda_pkgs.yml
 conda activate $(pwd)/$CONDA_ENV_NAME && \
 pip install --upgrade pip && \
-pip install jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_releases.html && \
+pip install jaxlib==0.1.69+cuda111 -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html && \
 cd $(pwd)/$CONDA_ENV_NAME/lib/python3.8/site-packages && \
 patch -p0 < $base_dir/AF4GUIFold/docker/openmm.patch && \
 cd $base_dir && \
