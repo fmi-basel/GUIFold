2e77f0267cf98b0edc0e32c716b802dbce5c3a08
georg.kempf <geox19@gmx.de>
2023-05-08 16:03:01 +0200

    Add dialog to select files or directories.

--- guifold/src/gui_dialogs.py
+++ guifold/src/gui_dialogs.py
@@ -17,7 +17,8 @@
 import logging
 import copy
 import pkg_resources
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtCore import Qt
 
 logger = logging.getLogger('guifold')
 
@@ -29,3 +30,41 @@ def message_dlg(title, text):
     return_value = dlg.exec()
 
     return return_value
+
+#From https://stackoverflow.com/a/64340482
+def open_files_and_dirs_dlg(parent=None, caption='', directory=None, 
+                        filter=None, initialFilter=None, options=None):
+    print(parent)
+    def updateText():
+        print("in function")
+        rows = view.selectionModel().selectedRows()
+        selected = []
+        for index in rows:
+            selected.append('"{}"'.format(index.data()))
+        lineEdit.setText(' '.join(selected))
+
+    dialog = QtWidgets.QFileDialog(parent, windowTitle=caption)
+    dialog.setFileMode(dialog.ExistingFiles)
+    if options:
+        dialog.setOptions(options)
+    dialog.setOption(dialog.DontUseNativeDialog, True)
+    if directory:
+        dialog.setDirectory(directory)
+    if filter:
+        dialog.setNameFilter(filter)
+        if initialFilter:
+            dialog.selectNameFilter(initialFilter)
+
+
+    dialog.accept = lambda: QtWidgets.QDialog.accept(dialog)
+
+    #recursively searching for the listview results in segfault
+    view = dialog.findChild(QtWidgets.QSplitter, "splitter").findChild(QtWidgets.QFrame, "frame").findChild(QtWidgets.QStackedWidget, "stackedWidget").findChild(QtWidgets.QWidget, "page").findChild(QtWidgets.QListView, "listView")
+
+    if dialog.exec_():
+        view.selectionModel().selectionChanged.connect(updateText)
+        lineEdit = dialog.findChild(QtWidgets.QLineEdit)
+        dialog.directoryEntered.connect(lambda: lineEdit.setText(''))
+        files = dialog.selectedFiles()
+    
+        return files
